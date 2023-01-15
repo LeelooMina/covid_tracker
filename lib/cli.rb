570fb8c3b7b166c2d130c3f70e6da3c241912f98
Crystal Campbell <blankpage@gmail.com>
2023-01-15 07:17:32 -0600

    Setting up files

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,10 +4,16 @@ class CLI
     end
 
     def run 
+        system('clear')
         greeting
+        end_program
     end
 
     def greeting
         puts "Welcome to the Covid-19 CLI Tracker!"
+    end
+
+    def end_program
+        puts "See you later!"
     end
 end
